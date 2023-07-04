cbb9d0aba4e31da08f364b07e872c61c46190b64
Nhatziet <92376163+trananhnhatviet@users.noreply.github.com>
2023-07-04 15:28:07 +0700

    Update multiply.py

--- examples/python/multiply.py
+++ examples/python/multiply.py
@@ -1,13 +1,3 @@
 #!/usr/local/bin/python
 
-import os
-from time import sleep
-from progress.bar import ChargingBar
-
-env = float(os.getenv("NUM"))
-user = float(input("number: "))
-
-for i in ChargingBar("Multiply", max=32, check_tty=False).iter(range(32)):
-    sleep(0.1)
-
-print(f"Done! {env} * {user} = {env * user}")
+print('Hello World')
